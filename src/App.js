6635cabeed79a0d971b4af4d0e6de81acc41ff0d
HarshR <harsh.repal2003@gmail.com>
2023-12-29 17:16:10 +0530

    Tour recommender components creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
+import data from './data';
+import Tours from './components/Tours'
 function App() {
-  return (
-   <div></div>
-  );
+   const [tours,setTours] = useState(data);
+    
+    function removeTour(id){
+      const newtours = tours.filter(tour => tour.id !== id);
+      setTours(newtours); 
+    }
+    
+
+
+  if(tours.length == 0){
+    return(
+        <div className='refresh'>
+          <h2>No Tours Left</h2>
+          <button onClick={() => setTours(data) }>Refresh
+          </button>
+        </div>
+    )
+  }
+
+    else{return (
+      <Tours tours = {tours} removeTour ={removeTour} ></Tours>);
+
+
+    }
 }
 
 export default App;
